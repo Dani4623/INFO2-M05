a7bf6a7b1528e9c8d88b4ab642dfcbd76ad5adfe
Dani4623 <daniferfer.06@gmail.com>
2025-10-10 11:05:25 +0200

    Add files via upload

---
+++ Version_0.1/FlightLib/FlightPlan.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightLib
+{
+    public class FlightPlan
+    {
+        // Atributos
+
+        string id; // identificador
+        Position currentPosition; // posicion actual
+        Position finalPosition; // posicion final
+        double velocidad;
+
+        // Constructures
+        public FlightPlan(string id, double cpx, double cpy, double fpx, double fpy, double velocidad)
+        {
+            this.id = id;
+            this.currentPosition = new Position(cpx, cpy);
+            this.finalPosition = new Position(fpx, fpy);
+            this.velocidad = velocidad;
+        }
+
+        // Metodos
+
+        public void SetVelocidad(double velocidad)
+        // setter del atributo velocidad
+        { this.velocidad = velocidad; }
+
+        public void Mover(double tiempo)
+        // Mueve el vuelo a la posición correspondiente a viajar durante el tiempo que se recibe como parámetro
+        {
+            //Calculamos la distancia recorrida en el tiempo dado
+            double distancia = tiempo * this.velocidad / 60;
+
+            //Calculamos las razones trigonométricas
+            double hipotenusa = Math.Sqrt((finalPosition.GetX() - currentPosition.GetX()) * (finalPosition.GetX() - currentPosition.GetX()) + (finalPosition.GetY() - currentPosition.GetY()) * (finalPosition.GetY() - currentPosition.GetY()));
+            double coseno = (finalPosition.GetX() - currentPosition.GetX()) / hipotenusa;
+            double seno = (finalPosition.GetY() - currentPosition.GetY()) / hipotenusa;
+
+            //Caculamos la nueva posición del vuelo
+            double x = currentPosition.GetX() + distancia * coseno;
+            double y = currentPosition.GetY() + distancia * seno;
+
+            Position nextPosition = new Position(x, y);
+
+            if (currentPosition.Distancia(nextPosition) < hipotenusa)
+            {
+                currentPosition = nextPosition;
+            }
+            else
+            {
+                currentPosition = finalPosition;
+            }
+        }
+
+        public bool EstaDestino()
+        {
+            bool resultado = false;
+            if (currentPosition == finalPosition)
+            {
+                resultado = true;
+            }
+            return resultado;
+        }
+
+        public void Restart(double cpx, double cpy)
+        {
+            currentPosition = new Position(cpx, cpy);
+        }
+
+        public double Distancia(double fpx, double fpy)
+        {
+            double hipotenusa = Math.Sqrt((fpx - currentPosition.GetX()) * (fpx - currentPosition.GetX()) + (fpy - currentPosition.GetY()) * (fpy - currentPosition.GetY()));
+            return hipotenusa;
+        }
+
+        public bool Conflicto(FlightPlan b, double distanciaSeguridad)
+        {
+            bool conflicto = false;
+            if (this.currentPosition.Distancia(b.currentPosition) < distanciaSeguridad)
+            {
+                conflicto = true;
+            }
+            return conflicto;
+        }
+
+        public void EscribeConsola()
+        // escribe en consola los datos del plan de vuelo
+        {
+            Console.WriteLine("******************************");
+            Console.WriteLine("Datos del vuelo: ");
+            Console.WriteLine("Identificador: {0}", id);
+            Console.WriteLine("Velocidad: {0:F2}", velocidad);
+            Console.WriteLine("Posición actual: ({0:F2},{1:F2})", currentPosition.GetX(), currentPosition.GetY());
+            if (this.EstaDestino())
+            {
+                Console.WriteLine("Ha llegado al destino.");
+            }
+            Console.WriteLine("******************************");
+        }
+    }
+}
---
+++ Version_0.1/SimulatorConsole/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FlightLib;
+
+namespace SimulatorConsole
+{
+    public class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            FlightPlanList lista = new FlightPlanList();
+
+            //DATOS DEL PRIMER VUELO
+            try
+            {
+                Console.WriteLine("Escribe el identificador del primer vuelo:");
+                //   string nombre = Console.ReadLine();
+                string identificador = Console.ReadLine(); ;
+
+                Console.WriteLine("Escribe la velocidad del primer vuelo:");
+                double velocidad = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Escribe las coordenadas de la posición inicial del primer vuelo, separadas por un blanco");
+                string linea = Console.ReadLine();
+                string[] trozos = linea.Split(' ');
+                double ix = Convert.ToDouble(trozos[0]);
+                double iy = Convert.ToDouble(trozos[1]);
+
+                Console.WriteLine("Escribe las coordenadas de la posición final, separadas por un blanco");
+                linea = Console.ReadLine();
+                trozos = linea.Split(' ');
+                double fx = Convert.ToDouble(trozos[0]);
+                double fy = Convert.ToDouble(trozos[1]);
+
+                FlightPlan plan_a = new FlightPlan(identificador, ix, iy, fx, fy, velocidad);
+
+                Console.WriteLine("Escribe el identificador del segundo vuelo:");
+                //   string nombre = Console.ReadLine();
+                identificador = Console.ReadLine(); ;
+
+                Console.WriteLine("Escribe la velocidad del segundo vuelo:");
+                velocidad = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Escribe las coordenadas de la posición inicial del segundo, separadas por un blanco");
+                linea = Console.ReadLine();
+                trozos = linea.Split(' ');
+                ix = Convert.ToDouble(trozos[0]);
+                iy = Convert.ToDouble(trozos[1]);
+
+                Console.WriteLine("Escribe las coordenadas de la posición final, separadas por un blanco");
+                linea = Console.ReadLine();
+                trozos = linea.Split(' ');
+                fx = Convert.ToDouble(trozos[0]);
+                fy = Convert.ToDouble(trozos[1]);
+
+                FlightPlan plan_b = new FlightPlan(identificador, ix, iy, fx, fy, velocidad);
+
+                lista.AddFlightPlan(plan_a);
+                lista.AddFlightPlan(plan_b);
+
+                int i = 0;
+                int ciclos = 100;
+                int tiempoCiclo = 10;
+                double distanciaSeguridad = 10;
+                bool fin = false;
+                while (i < ciclos)
+                {
+                    lista.GetFlightPlan(0).Mover(tiempoCiclo);
+                    lista.GetFlightPlan(1).Mover(tiempoCiclo);
+                    lista.GetFlightPlan(0).EscribeConsola();
+                    lista.GetFlightPlan(1).EscribeConsola();
+                    if (lista.GetFlightPlan(0).Conflicto(plan_b, distanciaSeguridad))
+                    {
+                        Console.WriteLine("Conflicto!!");
+                    }
+                    i = i + 1;
+                }
+                    
+                Console.ReadLine();
+
+
+            } 
+            catch (FormatException)
+            {
+                Console.WriteLine("Error de formato.");
+                Console.ReadLine();
+            }
+
+            
+
+            
+        }
+    }
+}
